ed9f0cc61948295997a7861638949fead3c94180
Moslem <moslem.ezati@gmail.com>
2023-10-17 11:05:30 +0330

    fix: Fix convert to english number

--- src/englishNumber.pip.ts
+++ src/englishNumber.pip.ts
@@ -1,38 +1,20 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-const persianNumbers = [
-    /۰/g,
-    /۱/g,
-    /۲/g,
-    /۳/g,
-    /۴/g,
-    /۵/g,
-    /۶/g,
-    /۷/g,
-    /۸/g,
-    /۹/g,
-  ],
-  arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g];
-
-function convertNumbers(input) {
-  for (const property in input) {
-    let str = input[property];
-    if (typeof str === 'string') {
-      for (let i = 0; i < 10; i++) {
-        str = str
-          .replace(persianNumbers[i], i.toString())
-          .replace(arabicNumbers[i], i.toString());
-      }
-      input[property] = str;
-    }
-  }
-
-  return input;
-}
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { convertNumbers } from './utils';
 
 @Injectable()
 export class EnglishNumberPip implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return convertNumbers(value);
+  transform(value: any) {
+    if (typeof value == 'string') {
+      return convertNumbers(value);
+    }
+    if (typeof value == 'object') {
+      for (const property in value) {
+        const str = value[property];
+        if (typeof str === 'string') {
+          value[property] = convertNumbers(str);
+        }
+      }
+    }
+    return value;
   }
 }
---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+export function convertNumbers(str: string) {
+  const persianNumbers = [
+    /۰/g,
+    /۱/g,
+    /۲/g,
+    /۳/g,
+    /۴/g,
+    /۵/g,
+    /۶/g,
+    /۷/g,
+    /۸/g,
+    /۹/g,
+  ];
+  const arabicNumbers = [
+    /٠/g,
+    /١/g,
+    /٢/g,
+    /٣/g,
+    /٤/g,
+    /٥/g,
+    /٦/g,
+    /٧/g,
+    /٨/g,
+    /٩/g,
+  ];
+  if (typeof str != 'string') return;
+  for (let i = 0; i < 10; i++) {
+    str = str
+      .replace(persianNumbers[i], i.toString())
+      .replace(arabicNumbers[i], i.toString());
+  }
+  return str;
+}
